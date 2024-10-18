3c7a2cb31b0746580f275f8258273614a8dd289d
Disordely <72545862+Disordely@users.noreply.github.com>
2024-10-18 16:32:45 +0100

    Create Startup.ps1

---
+++ Startup.ps1
@@ -0,0 +1,33 @@
+## Basic Menu
+Clear-Host
+write-host "USB Prod/Dev scripts selection"
+write-host "------------------------------"
+Write-Host "1 - [DEFAULT] Use Prod Script (From Azure)"
+Write-Host "2 - Engineering Team only - Use Dev (from Github)"
+Write-Host "Q - quit and restart"
+write-host "Note :  This is where deploy scripts run from and does not affect Windows DEV/UAT/PROD ring" 
+while($selection -notin @('1','2','Q'))
+    {
+    $selection = Read-Host "Enter selection [1,2,Q]"
+    }
+switch($selection.ToLower())
+    {
+    1
+        {
+        $Global:ScriptRootURL = "ProductionURL"
+        }
+    2
+        {
+        $Global:ScriptRootURL = "https://raw.githubusercontent.com/WoodneyUK/OOBE/main"
+        }
+    q
+        {
+        Write-Host "Restarting"
+        start-sleep 5
+        wpeutil reboot
+        }
+    }
+
+Write-Host "Setting URL to [$($ScriptRootURL)]"
+Invoke-RestMethod -uri "$($ScriptRootURL)\startup.ps1" | out-file $env:temp\startup.ps1 -force -encoding ascii
+& $env:temp\startup.ps1
